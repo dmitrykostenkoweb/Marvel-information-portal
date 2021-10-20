65762da94f78cb5a7e934c409572c316f14e9974
Dmitry Kostenko <60621984+dmitrykostenkoweb@users.noreply.github.com>
2021-10-20 19:37:28 +0200

    add random char

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/app/App";
 import "./style/style.scss";
 
-import MarvelService from "./services/MarvelService";
-
-const marService = new MarvelService();
-
-marService.getAllCharacters().then((res) =>
-  res.data.results.forEach((item) => {
-    console.log(item.name);
-  })
-);
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -10,15 +10,27 @@ export default class MarvelService {
 
     return await res.json();
   };
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this.__apiBase}characters?limit=9&offset=210&${this.__apiKey}`
     );
+    return res.data.results.map(this._transformCharacter)
   };
 
-  getCharacters = (id) => {
-    return this.getResource(
+  getCharacters = async (id) => {
+    const res = await this.getResource(
       `${this.__apiBase}characters/${id}?${this.__apiKey}`
     );
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
